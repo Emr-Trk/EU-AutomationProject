fc3b3ed7926e59c8e54cb7739c2c198d0de19953
AyseNur <caliskanturkayse@gmail.com>
2021-06-12 19:32:32 +0100

    doing more work after initally publishing to GitHub

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -4,6 +4,11 @@ public class Practice1 {
         System.out.println("EU5 is liking Git on IntelliJ");
 
         System.out.println("Work 1");
+
+        // Successfully pushed project to GitHub
+        //first IntelliJ automaticall created  remote repo
+        //created connection between local repo and remote repo
+        //pushed local commits to remote repo
     }
 
 }
